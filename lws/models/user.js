de04bf4cd3f14d1099665342ebf001b6611b9ce0
immose93 <immoses93@gmail.com>
2020-09-02 14:39:36 +0900

    user 모델 수정

--- lws/models/user.js
+++ lws/models/user.js
@@ -1,52 +1,45 @@
 module.exports = function(sequelize, DataTypes){
+    // 모델 정의
     let user = sequelize.define("User", {
         // ID
-        userID: {
-            filed: "user_id",
-            type: DataTypes.VARCHAR(30),
+        user_id: {
+            type: DataTypes.STRING(30),
             unique: true,
             allowNull: false
         },
         // 비밀번호
         password: {
-            filed: "password",
-            type: DataTypes.VARCHAR(30),
+            type: DataTypes.STRING(30),
             allowNull: false
         },
         // 이름
         name: {
-            filed: "name",
-            type: DataTypes.VARCHAR(30),
+            type: DataTypes.STRING(30),
             allowNull: false
         },
-        // 생일
+        // 생일(YYYY-MM-DD)
         birthday: {
-            filed: "birthday", 
             type: DataTypes.DATE,
             allowNull: false
         },
         // 성별
         gender: {
-            filed: "gender",
-            type: DataTypes.CHAR(1),
+            type: DataTypes.STRING(1),
             allowNull: false
         },
         // 휴대전화
         phone: {
-            filed: "phone",
-            type: DataTypes.VARCHAR(20),
+            type: DataTypes.STRING(20),
             allowNull: false
         },
         // 이메일
         email: {
-            filed: "email",
             type: DataTypes.STRING(320),
             allowNull: false
         },
-        // 가입일(YYYYMMDDHHMMSS)
-        joined: {
-            filed: "joined",
-            type: DataTypes.TIMESTAMP,
+        // 가입일(YYYY-MM-DD)
+        createdAt: {
+            type: DataTypes.DATE,
             allowNull: false
         }
     }, {
